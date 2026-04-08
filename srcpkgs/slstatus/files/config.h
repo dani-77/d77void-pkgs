5e608ca2dadc92ddc303cbab679b66eba44cbe3e
Daniel Azevedo <daniazevedo77@posteo.net>
2026-04-08 11:36:00 +0100

    Void pkgs updated

--- srcpkgs/slstatus/files/config.h
+++ srcpkgs/slstatus/files/config.h
@@ -13,11 +13,11 @@ static const char unknown_str[] = "n/a";
  * function            description                     argument (example)
  *
  * battery_perc        battery percentage              battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
+ *                                                     NULL on OpenBSD
  * battery_state       battery charging state          battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
+ *                                                     NULL on OpenBSD
  * battery_remaining   battery remaining HH:MM         battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
+ *                                                     NULL on OpenBSD
  * cpu_perc            cpu usage in percent            NULL
  * cpu_freq            cpu frequency in MHz            NULL
  * datetime            date and time                   format string (%F %T)
@@ -52,8 +52,6 @@ static const char unknown_str[] = "n/a";
  * temp                temperature in degree celsius   sensor file
  *                                                     (/sys/class/thermal/...)
  *                                                     NULL on OpenBSD
- *                                                     thermal zone on FreeBSD
- *                                                     (tz0, tz1, etc.)
  * uid                 UID of current user             NULL
  * uptime              system uptime                   NULL
  * username            username of current user        NULL
@@ -63,16 +61,5 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	//{ wifi_essid, "[  on %s"   , "wls3" },/
-	//{ wifi_perc, " %s%%]" , "wls3"},/
-	{ hostname, "[ %s]", NULL },
-	{ cpu_perc, "[ %s%%]", NULL },
-	{ ram_perc, "[ %s%%]", NULL },
-	{ keymap, "[ %s",  NULL },
-	{ keyboard_indicators, " %s]", "(c?/n?)" },
-	//{ battery_perc, "[ %s%%", "BAT0" },/
-	//{ battery_state, " %s",	"BAT0" },/
-	//{ battery_remaining, " %s]", "BAT0" },/
-	//{ datetime, "%s", "%a %b %d %r" },/
-	{ datetime, "[ %s]", "%A, %d/%B/%Y -  %T" },
+	{ datetime, "%s",           "%F %T" },
 };
